6af1947823c43a9defe4de59c8ae497d0c2d1e8e
Jia Wong <jpw_4@msn.com>
2017-07-10 12:29:26 +0100

    changed CheckBoxes to RadioButtons

--- app/src/main/java/com/example/user/foodtrackerproject/AddMealActivity.java
+++ app/src/main/java/com/example/user/foodtrackerproject/AddMealActivity.java
@@ -1,7 +1,6 @@
 package com.example.user.foodtrackerproject;
 
 import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,33 +11,28 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
-import android.text.format.DateFormat;
 
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-import static android.R.attr.checked;
-
-public class AddMealActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+public class AddMealActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener{
 
     EditText foodNameEditor;
-    ArrayList<String> addArray = new ArrayList<String>();
+    ArrayList<String> inputArray = new ArrayList<String>();
 
     Button date_time;
     TextView show_date_time;
-    CheckBox breakfastCheckbox, lunchCheckbox, dinnerCheckbox, snackCheckbox, beverageCheckbox;
+
+    RadioButton radio_breakfast, radio_lunch, radio_dinner, radio_snack, radio_beverage;
     DatePickerDialog datePicker;
-    TimePickerDialog timePicker;
-    int day, month, year, hour, minute;
-    int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
+    int day, month, year;
+    int dayFinal, monthFinal, yearFinal;
 
     ArrayAdapter adapter;
 
@@ -54,19 +48,18 @@ public class AddMealActivity extends AppCompatActivity implements AdapterView.On
         show_date_time = (TextView) findViewById(R.id.show_date_time);
         date_time = (Button) findViewById(R.id.date_time_picker);
 
-        breakfastCheckbox = (CheckBox) findViewById(R.id.breakfast);
-        lunchCheckbox = (CheckBox) findViewById(R.id.lunch);
-        dinnerCheckbox = (CheckBox) findViewById(R.id.dinner);
-        snackCheckbox = (CheckBox) findViewById(R.id.snack);
-        beverageCheckbox = (CheckBox) findViewById(R.id.beverage);
+        radio_breakfast = (RadioButton)findViewById(R.id.radio_breakfast);
+        radio_lunch = (RadioButton)findViewById(R.id.radio_lunch);
+        radio_dinner = (RadioButton)findViewById(R.id.radio_dinner);
+        radio_snack = (RadioButton)findViewById(R.id.radio_snack);
+        radio_beverage = (RadioButton)findViewById(R.id.radio_snack);
+
         Calendar cal = Calendar.getInstance();
         year = cal.get(Calendar.YEAR);
         month = cal.get(Calendar.MONTH);
         day = cal.get(Calendar.DAY_OF_MONTH);
-        Time time = Time.getInstance();
 
         datePicker = new DatePickerDialog(AddMealActivity.this, AddMealActivity.this, year, month, day);
-        timePicker = new TimePickerDialog(AddMealActivity.this, AddMealActivity.this, hour, minute, DateFormat.is24HourFormat(this));
 
         date_time.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,13 +80,22 @@ public class AddMealActivity extends AppCompatActivity implements AdapterView.On
                 ArrayList<MealTime> selectedMealTimes = AddMealActivity.this.getSelectedMealTimes();
 
                 Date date = AddMealActivity.this.getSelectedDate();
-                Date time = AddMealActivity.this.getSelectedTime();
                 EatFood eatFoodEvent = new EatFood();
                 eatFoodEvent.setFood(new Food(selectedMealName, 0 ,0,0));
                 eatFoodEvent.setMealTime(selectedMealTimes.get(0));
                 eatFoodEvent.setUser(new User("Jia", "Female", 56));
             }
         });
+    }
+
+    public boolean ifNoInput(EditText editText){
+        if(foodNameEditor == null){
+            editText.setError(editText.getHint()+"required field");
+            return false;
+        } else {
+            editText.setError(null);
+            return true;
+        }
     }
 
     @Override
@@ -103,19 +105,7 @@ public class AddMealActivity extends AppCompatActivity implements AdapterView.On
         dayFinal = i2;
 
         Calendar cal = Calendar.getInstance();
-        hour = cal.get(Calendar.HOUR_OF_DAY);
-        minute = cal.get(Calendar.MINUTE);
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(AddMealActivity.this, AddMealActivity.this, hour, minute, DateFormat.is24HourFormat(this));
-        timePickerDialog.show();
-    }
-
-    @Override
-    public void onTimeSet(TimePicker timePicker, int i, int i1) {
-        hourFinal = i;
-        minuteFinal = i1;
-
-        show_date_time.setText(dayFinal + "/" + monthFinal + "/" + yearFinal + " " + hourFinal + ":" + minuteFinal);
+        show_date_time.setText(dayFinal + "/" + monthFinal + "/" + yearFinal);
     }
 
     @Override
@@ -139,30 +129,24 @@ public class AddMealActivity extends AppCompatActivity implements AdapterView.On
                 this.datePicker.getDatePicker().getDayOfMonth());
     }
 
-    private Date getSelectedTime() {
-        return new Time(this.timePicker.getHour();
-                this.timePicker.getMinute();
-    }
-
     private ArrayList<MealTime> getSelectedMealTimes() {
         ArrayList<MealTime> selectedMealTimes = new ArrayList<MealTime>();
 
-        if (breakfastCheckbox.isChecked()) {
+        if (radio_breakfast.isChecked()) {
             selectedMealTimes.add(new MealTime("Breakfast"));
         }
-        if (lunchCheckbox.isChecked()) {
+        if (radio_lunch.isChecked()) {
             selectedMealTimes.add(new MealTime("Lunch"));
         }
-        if (dinnerCheckbox.isChecked()) {
+        if (radio_dinner.isChecked()) {
             selectedMealTimes.add(new MealTime("Dinner"));
         }
-        if (snackCheckbox.isChecked()) {
+        if (radio_snack.isChecked()) {
             selectedMealTimes.add(new MealTime("Snack"));
         }
-        if (beverageCheckbox.isChecked()) {
+        if (radio_beverage.isChecked()) {
             selectedMealTimes.add(new MealTime("Beverage"));
         }
-
         return selectedMealTimes;
     }
 
